7b91fa6afd622ede96b68b052a15c1f869e5a8e3
Kalena Almand <kalmand1057@gmail.com>
2020-10-21 16:27:01 -0500

    created connection in server.js

--- server.js
+++ server.js
@@ -1,1 +1,106 @@
-var app = require("express");
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mysql = require("mysql");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "!057Captain",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+// Use Handlebars to render the main html
+app.get("/", function(req, res) {
+  connection.query("SELECT * FROM burgers;", function(err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    res.render("index", { burgers: data });
+  });
+});
+
+// Create a new movie
+app.post("/api/burgers", function(req, res) {
+  connection.query("INSERT INTO burgers (burger) VALUES (?)", [req.body.burger_name], function(err, result) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    // Send back the ID of the new movie
+    res.json({ id: result.insertId });
+    console.log({ id: result.insertId });
+  });
+});
+
+// Retrieve all movies
+app.get("/api/burgers", function(req, res) {
+  connection.query("SELECT * FROM burgers;", function(err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    res.json(data);
+  });
+});
+
+// Update a burger
+app.put("/api/burgers/:id", function(req, res) {
+  connection.query("UPDATE burgers SET burger = ? WHERE id = ?", [req.body.burger_name, req.params.id], function(err, result) {
+    if (err) {
+      // If an error occurred, send a generic server failure
+      return res.status(500).end();
+    }
+    else if (result.changedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+
+  });
+});
+
+// Delete a movie
+app.delete("/api/burgers/:id", function(req, res) {
+  connection.query("DELETE FROM burgers WHERE id = ?", [req.params.id], function(err, result) {
+    if (err) {
+      // If an error occurred, send a generic server failure
+      return res.status(500).end();
+    }
+    else if (result.affectedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+
+  });
+});
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
