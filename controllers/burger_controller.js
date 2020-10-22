1a18a5130cb6f1c1516567c38704eb1cb2df04b7
Kalena Almand <kalmand1057@gmail.com>
2020-10-21 22:20:03 -0500

    fixes to burger_controller.js in controllers folder

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -14,29 +14,25 @@ router.get("/", function(req, res) {
     });
   });
   
-  router.post("/api/burgers", function(req, res) {
+  router.post("/burgers", function(req, res) {
     burger.create(["Burger Name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
     });
   });
   
-  router.put("/api/burgers/:id", function(req, res) {
+  router.put("/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
   
-    console.log("condition", condition);
+    //console.log("condition", condition);
   
-    cat.update(
+    burger.update(
       {
-        devoured: req.body.devoured
+        devoured: true
       },
       condition,
-      function(result) {
-        if (result.changedRows === 0) {
-          // If no rows were changed, then the ID must not exist, so 404
-          return res.status(404).end();
-        }
-        res.status(200).end();
+      function(data) {
+        res.redirect("/");
   
       }
     );
