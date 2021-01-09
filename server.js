c4cf10cbc3d27865870f16054304f3d4c9a1430c
Kalena Almand <kalmand1057@gmail.com>
2021-01-09 12:14:20 -0600

    heroku fix

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get("/", function(req, res) {
 
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+app.listen(process.env.PORT || 8080, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
